2551abba0fb09c9beee4c1325fbc82f42966272b
gforney <gforney@gmail.com>
2016-07-11 15:26:47 -0400

    build: use makefile instead of makefile2 in windows and osx build scripts

--- Build/intel_win_64_db/make_radcal.bat
+++ Build/intel_win_64_db/make_radcal.bat
@@ -4,5 +4,5 @@ Title Building debug radcal for 64 bit Windows
 call ..\Scripts\setup_intel_compilers.bat
 
 erase *.obj *.mod
-make VPATH="../../Source" -f ..\makefile2 intel_win_64_db
+make VPATH="../../Source" -f ..\makefile intel_win_64_db
 pause
